de499398709d21ce3d86ef158c481f00c6f723c0
jhonVitor-rs <joaovitor.jvrs6@gmail.com>
2025-10-23 17:41:11 -0300

    feat: configure auth guard

--- packages/core/src/commands/auth.ts
+++ packages/core/src/commands/auth.ts
@@ -3,5 +3,7 @@ export const AUTH_PATTERNS = {
   LOGIN_USER: { cmd: 'login_user' },
   REFRESH_TOKEN: { cmd: 'refresh_token' },
   CONFIRM_EXISTANCE_USER: { cmd: 'confirm_existance_user' },
-  GET_USER_INFORMATION: { cmd: 'get_user_information' }
+  GET_USER_INFORMATION: { cmd: 'get_user_information' },
+  GET_ALL_USER: { cmd: 'get_all_user' },
+  VALIDATE_ACCESS_TOKEN: { cmd: 'validate_token' }
 }
